5b85b9f59fc60065073bb12d89b7c075f109bac5
Assassin-PL <michael.za1995@gmail.com>
2023-11-12 23:24:41 +0100

    typy danych lekcja 3

--- Projekt Wono/Kod/main.go
+++ Projekt Wono/Kod/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 var JANUSZ string = "TwojaStara"
@@ -13,12 +14,24 @@ var (
 
 func main() {
 	i := 42
+	var n bool = true
 	var j int = 27
 	k := 99
+	var m string = "NULL"
+	o := 1 == 2 // inicjalizacja zmiennych boolowskich
+	m = strconv.Itoa(k)
 	fmt.Println(i, j, k)
 	fmt.Printf("%v, %T", i, i)
 	fmt.Printf("\n")
 	fmt.Printf("%v, %T", a, a)
 	fmt.Printf("\n")
+	fmt.Printf("%v, %T", k, k)
+	fmt.Printf("\n")
 	fmt.Print(actorName)
+	fmt.Printf("\n")
+	fmt.Printf("%v, %T", m, m)
+	fmt.Print(n)
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+	fmt.Printf("%v, %T", o, o)
 }
